42931f4352d0d80769aa7139f3028dfa2d96857b
Zach <zach@wayfaring.io>
2016-04-13 11:22:25 -0700

    Updated files

--- final/server.R
+++ final/server.R
@@ -2,12 +2,15 @@ library(shiny)
 require(dplyr)
 require(lubridate)
 require(data.table)
+require(DT)
 
+#use data.table fread to pull extract the data and set NA strings as 0 values
 dat <- fread("data/fitbit_data.csv", stringsAsFactors = FALSE, na.strings = "0")
-dat$Day <- weekdays(x = as.Date(dat$Date, "%m/%d/%Y"
-                                ,label = TRUE, abbr = FALSE))
-dat$Date <- as.Date(dat$Day, "%m/%d/%Y")
+dat$Date <- as.Date(dat$Date, format = "%m/%d/%Y")
+dat$Day <- weekdays(dat$Date, abbreviate = FALSE)
+dat$Month <- months(dat$Date, abbreviate = FALSE)
 dat$Steps <- as.numeric(sub(",","",dat$Steps))
+dat$Steps.Ordered <- sort(dat$Steps, na.last = TRUE)
 dat$`Calories Burned` <- as.numeric(sub(",","",dat$`Calories Burned`))
 dat$`Minutes Sedentary` <- as.numeric(sub(",","",dat$`Minutes Sedentary`))
 dat$`Activity Calories` <- as.numeric(sub(",","",dat$`Activity Calories`))
@@ -27,13 +30,18 @@ shinyServer(function(input, output) {
          , ylab = "Frequency"
          , main = "Histogram of Steps")
     
+    #summary statistics for all steps
     m <- mean(dat$Steps, na.rm = TRUE)
     s <- sqrt(var(dat$Steps, na.rm = TRUE))
     md <- median(dat$Steps, na.rm = TRUE)
+    n <- length(dat$Steps)
+    
+    #parameters for plotting normal curve overlay
     xfit <- seq(min(dat$Steps, na.rm = TRUE)
                 , max(dat$Steps, na.rm = TRUE), length = 40)
     yfit <- dnorm(xfit, mean = m, sd = s)
     yfit2 <- yfit*diff(h$mids[1:2])*length(dat$Steps)
+    dat$Steps.Ordered <- sort(dat$Steps, na.last = TRUE)
     
    if(input$checkCurve == TRUE) {
           lines(xfit, yfit2, col = "darkblue", lwd = 2)
@@ -44,10 +52,20 @@ shinyServer(function(input, output) {
     if(input$checkMed == TRUE) {
       abline(v = md, lwd = 2, col = "red") 
      }#end plot-median-if
-    })#end renderPlot
- 
-output$table <- renderTable({
-  if(input$tableButton == 0) {return()}
+    })#end HistrenderPlot
+  
+  output$cdfPlot <- renderPlot({
+  plot(dat$Steps.Ordered, (1:n)/n, type = 's', ylim = c(0,1), ylab = ''
+       , xlab = 'Ordered Sum of Steps'
+       , main = 'Cumulative Distribution of Steps')
+  abline(v = 13500, h = .75, col = 'red', lwd = 1) #plot 3rd quartile
+  legend(13500, 0.3, '3rd Quartile = 13,500', box.lwd = 0, cex = .75)
+  legend(11490, .5, 'Median = 11,490', box.lwd = 0, cex = .75)
+  mtext(text=expression(hat(F)[n](x)), side = 2, line = 2.5) #hat ylab
+}) #end CDFRenderPlot
+
+output$tableDays <- renderTable({
+  if(input$tableButton%%2 == 0) {return()}
   else{
     dat %>%
       group_by(Day) %>%
@@ -57,9 +75,26 @@ output$table <- renderTable({
                 , min = min(Steps, na.rm = TRUE)
                 , max = max(Steps, na.rm = TRUE)
                 , med = median(Steps, na.rm = TRUE))
-  }
+  }#end else
+})#end tableDays
+
+output$tableMonths <- renderTable({
+  if(input$tableButton2%%2 == 0) {return ()}
+  else{
+    dat %>%
+      group_by(Month) %>%
+      summarise(., total = sum(Steps, na.rm = TRUE)
+                , avg = mean(Steps, na.rm = TRUE)
+                , min = min(Steps, na.rm = TRUE)
+                , max = max(Steps, na.rm = TRUE)
+                , stdev = sqrt(var(Steps, na.rm = TRUE))
+      ) #end monthlyTable
+  }#end else
+})#end tableMonths
+
+output$`raw-data` <- renderDataTable({
+  datatable(dat)
 })
-        
   
 })#end shinyServer
 
--- final/ui.R
+++ final/ui.R
@@ -10,39 +10,46 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       helpText("Select some of the features of the histogram."),
-      
+      #call to server to change the bin range of the histogram
       sliderInput("bins", label = h4("Number of bins: ")
                   , min = 5
                   , max = 50
                   , value = 10),
+      #call to server to change the color of plot lines
       radioButtons("radio-color", helpText(h5("Select a color for density plot.")),
                    choices = list("Salmon" = "salmon", "Black" = "black"
                                   ,"Red" = "red", "Dark Blue" = "darkblue"
                                   , "Dark Grey" = "darkgrey")
                    ,selected = "salmon"),
       helpText(h5("Select some plot overlays")),
-      checkboxInput("checkCurve", label = "Curve", value = FALSE), 
-      checkboxInput("checkMean", label = "Mean", value = FALSE),
-      checkboxInput("checkMed", label = "Median", value = FALSE),
-      helpText(h5("Generate daily summary statistics?")),
-      actionButton("tableButton", label = "Generate")
+      checkboxInput("checkCurve", label = "Curve", value = FALSE),  #plot curve
+      checkboxInput("checkMean", label = "Mean", value = FALSE), #plot mean line
+      checkboxInput("checkMed", label = "Median", value = FALSE), #plot med line
+      helpText(h5("Generate time series summary statistics")),
+      actionButton("tableButton", label = "Daily"),
+      actionButton("tableButton2", label = "Monthly")#generate tbl of stats
     ),#end sideBarPanel
     
   # Show a plot of the generated distribution
     mainPanel(
       tabsetPanel(
-        tabPanel(p(icon("line-chart"), "Visualize Data"),
+        tabPanel(p(icon("bar-chart"), "Visualize Data"),
                  plotOutput("histPlot", height = "400px"),
-                 tableOutput("table")   
+                 plotOutput("cdfPlot", height = "442px"),
+                 tableOutput("tableDays"),
+                 tableOutput("tableMonths")
         ), #end viz tab
-        tabPanel(p(icon("about"), "About"),
+        tabPanel(p(icon("file-text-o"), "About"),
                  h1("About"),
-                 h5("This application was built using actual Fitbit step data generated
+                 h5("This application was built using my Fitbit step data generated
            by a Fitbit Charge HR for the period 01/01/2016 through 04/08/2016. 
 As a user you simply need to click the buttons to change the color scheme of the
 histogram or to plot abline summary statistics. The 'generate' button will plot
            a data table of the Daily step statistics.")
-                 )
+                 ),#end about tabPanel
+        tabPanel(p(icon("table"), "Raw Data"),
+                 dataTableOutput("raw-data")
+                 )#end rawdataTabPanel
         )#end tabsetPanel
    )#End mainPanel
   )#End sidebarLayout
